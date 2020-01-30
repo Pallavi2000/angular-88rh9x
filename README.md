4b20531ba2016dacdf89141e5434c40bbf2e12f6
Pallavi2000 <hmpsharma@gmail.com>
2020-01-30 11:48:23 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-88rh9x
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-88rh9x)
---
+++ src/app/app.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { TeamComponent } from './team/team.component';
+
+import { RouterModule,Routes } from '@angular/router';
+const routes:Routes = [
+  {
+    path:"",
+    component:HomeComponent,
+    pathMatch:"full"
+  },
+  {
+    path:"home",
+    component:HomeComponent
+  },
+  {
+    path:"team",
+    component:TeamComponent
+  }
+
+]
+
+@NgModule({
+  imports: [ 
+    BrowserModule, FormsModule,
+    RouterModule.forRoot(routes) ],
+  declarations: [ AppComponent,HeaderComponent,HomeComponent,TeamComponent],
+  bootstrap:    [ AppComponent ]
+})
+export class AppModule {
+
+ }
